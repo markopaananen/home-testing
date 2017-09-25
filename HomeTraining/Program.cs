54ffd5e7c1faab92484ebb6d46ff5f1b9768a42d
markopaananen@student.saimia.fi <marko.paananen@student.saimia.fi>
2017-09-25 21:51:39 +0300

    Home testing for function calls - done.

--- HomeTraining/Program.cs
+++ HomeTraining/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 
-namespace HomeTraining
+
+
+namespace task12
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
+            // Prompt user for input
+
+            Console.Write("Get Random number (use dot to separate desimals): ");
+
+            string userInput = Console.ReadLine();
+
+            double.TryParse(userInput, out double inputnumber1);
+
+            Console.Write("Get another random number: ");
+
+            userInput = Console.ReadLine();
+
+            double.TryParse(userInput, out double inputnumber2);
+
+            Console.WriteLine("Given number {0} was lesser.\n", MinNumber(inputnumber1, inputnumber2));
+
+            Console.WriteLine("Press any key to continue...");
+
+            Console.ReadKey();
+
+
+
+            double MinNumber(double number1, double number2)
+
+            {
+
+                // Return the lesser number
+
+                return Math.Min(number1, number2);
+
+            }
+
         }
+
     }
+
 }
---
+++ trainingFunctionsTask1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace trainingFunctionsTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, lasketaan kolmion tai suorakaiteen ala\n");
+            Console.WriteLine("----------------------------------------------------\n");
+            Console.WriteLine("Suorakaiteen vai kolmion ala (S/K))\n");
+            string userInput = Console.ReadLine();
+            int uAla = 0;
+
+            Console.Write("Anna kanta:\n");
+            string userKanta = Console.ReadLine();
+            //int valueKanta = int.TryParse(userKanta, out int uKanta);
+             int.TryParse(userKanta, out int uKanta);
+
+            Console.WriteLine("Anna korkeus\n");
+            string userKorkeus = Console.ReadLine();
+            //int valueKorkeus = int.TryParse(userKorkeus, out int uKorkeus);
+            int.TryParse(userKorkeus, out int uKorkeus);
+
+            Console.WriteLine ("Pinta-ala :{0}", calcArea(userInput, uKanta, uKorkeus, uAla));
+            
+            //Console.WriteLine("Vastaus alan suuruuteen on : {0} ", calcArea((userInput, uKanta, uKorkeus));
+            Console.WriteLine("Press any key to continue,,,");
+            Console.ReadKey();
+
+
+
+            int calcArea(string UI, int Kanta, int Korkeus, int tulostaAla)
+            {
+                //int tulostaAla = 0;
+                if (UI == "S")
+                {
+                    tulostaAla  = Kanta * Korkeus;  
+                }
+                else
+                {
+                    tulostaAla = (Kanta * Korkeus) / 2;
+
+                }
+                return tulostaAla;
+            }
+        }
+    }
+}
